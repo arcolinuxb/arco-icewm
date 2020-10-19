3ee3bab7d96aa1af98107decc34a4b7132061b39
Erik Dubois <erik.dubois@gmail.com>
2020-10-19 06:42:29 +0200

    bump nr for new calamares config

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="icewm"
 	lightdmDesktop="icewm-session"
 
-	arcolinuxVersion='v20.11.8'
+	arcolinuxVersion='v20.11.9'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
