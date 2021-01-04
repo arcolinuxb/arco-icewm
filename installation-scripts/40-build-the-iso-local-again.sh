5e9ee95dad14e963497459c703de7a5c9bb1afdd
Erik Dubois <erik.dubois@gmail.com>
2021-01-04 15:47:38 +0100

    version 21.01.8

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="icewm"
 	lightdmDesktop="icewm-session"
 
-	arcolinuxVersion='v21.01.7'
+	arcolinuxVersion='v21.01.8'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
