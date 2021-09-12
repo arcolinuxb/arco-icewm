dbc7f9b6f793063208934ebf5d212cdda3c6e24a
Erik Dubois <erik.dubois@gmail.com>
2021-09-12 08:06:04 +0200

    v21.11.1

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="icewm"
 	dmDesktop="icewm-session"
 
-	arcolinuxVersion='v21.09.11'
+	arcolinuxVersion='v21.11.1'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
