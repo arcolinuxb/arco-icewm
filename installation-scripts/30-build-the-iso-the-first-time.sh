2c5f6c8205be2d7c3878380a2df9560544b6e7aa
arcolinuxz <arcolinuxinfo@gmail.com>
2022-01-23 08:18:33 +0100

    bump

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="icewm"
 	dmDesktop="icewm-session"
 
-	arcolinuxVersion='v22.03.02'
+	arcolinuxVersion='v22.03.03'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
