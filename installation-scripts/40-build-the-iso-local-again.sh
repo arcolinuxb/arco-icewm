51ce98fa951ca1ce7dca43fc9e240caabc9e5192
arcolinuxz <arcolinuxinfo@gmail.com>
2022-07-18 16:14:17 +0200

    arc theme

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="icewm"
 	dmDesktop="icewm-session"
 
-	arcolinuxVersion='v22.09.02'
+	arcolinuxVersion='v22.09.03'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
