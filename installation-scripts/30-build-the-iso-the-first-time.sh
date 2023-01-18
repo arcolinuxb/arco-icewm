baae711d978e397d6178d4c9d5be6fa46cd56863
arcolinuxz <arcolinuxinfo@gmail.com>
2023-01-18 07:22:21 +0100

    bump nr for release

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="icewm"
 	dmDesktop="icewm-session"
 
-	arcolinuxVersion='v23.02.01'
+	arcolinuxVersion='v23.02.02'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
