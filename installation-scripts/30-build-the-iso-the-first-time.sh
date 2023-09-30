4f8851eaa8043704a7bb6887e7e694ab189c8ab7
Erik Dubois <erik.dubois@gmail.com>
2023-09-30 06:17:41 +0200

    bump

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="icewm"
 	dmDesktop="icewm-session"
 
-	arcolinuxVersion='v23.10.01'
+	arcolinuxVersion='v23.11.01'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
